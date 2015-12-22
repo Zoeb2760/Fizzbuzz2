7bd225d3b6121d56e5d431995660155063aed5e7
ZB <Zoeb2760@gmail.com>
2015-12-22 08:43:28 -0500

    Updated initial prompt info

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 $(document).ready(function() { 
 
 
-   var game = Number(prompt("Please enter a number to see numbers divisble by 3 written as Fizz, numbers divisible by 5 as Buzz and numbers divisble by 5 and 3 as Fizzbuzz?"));
+   var game = Number(prompt("Please enter a number to see all numbers up to the entered number that are divisble by 3 written as Fizz, numbers divisible by 5 as Buzz and numbers divisble by 5 and 3 as Fizzbuzz?"));
  // Create a function for the action
  //game= Number(game);
      //if (game % 1 !=0) {break};
